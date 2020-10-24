fb5787668b234dafd1f6f043d15e61e9667c8638
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-10-24 00:28:26 -0400

    add sentence framework

--- project.js
+++ project.js
@@ -1,1 +1,25 @@
-console.log('yo whaddup world aha');
+
+const sentences = [];
+
+function createSentences(){
+    
+}
+
+function createMessage(){
+    const good =['Good things will happen for you', 'I like the way things are going', 'ur a real cutie'];
+    const bad = ['Bad things are heading your way', 'Better be wary of people today', 'u will be hurting soon'] ;
+    const neutral = ['Just another day', 'you should eat', 'water is good for you'];
+    const message = [];
+    for(i=0;i<3;i++){
+        const rando = Math.floor(Math.random()*3);
+        if(rando === 1){
+            message.push(good[rando]);
+        }
+        else if(rando === 2){
+            message.push(bad[rando]);
+        }
+        else{
+            message.push(neutral[rando]);
+        }
+    }
+};
